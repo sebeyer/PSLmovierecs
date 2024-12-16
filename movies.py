8909959d03e9cffd495d3d1fc4dcd4fd193767ba
sebeyer <43526015+sebeyer@users.noreply.github.com>
2024-12-16 12:08:58 +0100

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
 
 st.write('test')
+
+# load Rmat.csv
+df = pd.read_csv('/Users/sebeyer/Desktop/MCS/Practical statistical learning/projects/project 4/Rmat.csv')
